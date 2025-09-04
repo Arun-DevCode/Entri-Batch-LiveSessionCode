5abb778eed85dd0ed3e1fd88be90b16b5176036c
Arun-DevCode <arunkumar.d@entri.me>
2025-09-04 10:16:47 +0530

    Update Student mark UI

--- B37 Session/reactjs/src/main.jsx
+++ B37 Session/reactjs/src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router";
+import AppRouter from "./routes/routes.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={AppRouter} />
+  </StrictMode>
+);
