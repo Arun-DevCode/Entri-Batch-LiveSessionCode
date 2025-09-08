8288c469c6509a60baaf56878fae2822c4b13a50
Arun-DevCode <arunkumar.d@entri.me>
2025-09-08 11:45:07 +0530

    Bootstrap

--- B37 Session/reactjs/src/components/StudentList.jsx
+++ B37 Session/reactjs/src/components/StudentList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-function StudentList({ data }) {
+function StudentList({data}) {
+  
   return (
     <div className="max-w-4xl mx-auto p-4">
       <div className="space-y-3">
