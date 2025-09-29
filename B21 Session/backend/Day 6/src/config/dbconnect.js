1638fa4cc217514cac2c986b7e96918670719bb2
Arun-DevCode <arunkumar.d@entri.me>
2025-09-29 11:43:00 +0530

    Code updated

--- B21 Session/backend/Day 6/src/config/dbconnect.js
+++ B21 Session/backend/Day 6/src/config/dbconnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectwithDB() {
   try {
     await mongoose
-      .connect("Mongodb Atlas URL")
+      .connect("mongodb://localhost:27017/Entri-session")
       .then(() => console.log("Database connected!"))
       .catch((err) => {
         throw new Error("Failed to connect with database!", err);
