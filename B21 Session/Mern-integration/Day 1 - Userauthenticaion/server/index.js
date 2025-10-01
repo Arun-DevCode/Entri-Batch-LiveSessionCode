f26c5ba0f555e6631de07a863331af149cde0fea
Arun-DevCode <arunkumar.d@entri.me>
2025-10-01 10:56:24 +0530

    Protected Routes in Server

--- B21 Session/Mern-integration/Day 1 - Userauthenticaion/server/index.js
+++ B21 Session/Mern-integration/Day 1 - Userauthenticaion/server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // File Imports
 const DBConnector = require("./config/db");
 const UserModel = require("./models/users");
+const authenticate = require("./middleware/authenticate");
 
 //App setup
 const app = express();
@@ -54,25 +56,60 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const UserData = await UserModel.find({ email: email });
 
-    // Validation
-    if (!UserData) {
+    // validation
+    if (!email || !password) {
+      return res.status(404).json({ message: "All fields are required!" });
+    }
+
+    // Validate on User exist
+    const isExistUser = await UserModel.findOne({ email: email });
+    if (!isExistUser) {
       return res
         .status(404)
-        .json({ success: false, message: "No User data found!" });
+        .json({ message: "User not found.Please go to register!" });
     }
+
+    // Generate secret key
+    const payload = {
+      username: isExistUser.username,
+      email: isExistUser.email,
+    };
+    const token = await jwt.sign(payload, "entri@2025", { expiresIn: "24hr" });
+
     // Success res to client
     res.status(200).json({
-      success: false,
-      message: "User data fetched success..",
-      Users: UserData,
+      success: true,
+      message: "User loggedin success..",
+      accessToken: token,
     });
   } catch (error) {
     res.status(500).json({ success: false, message: "Internal Server Error!" });
   }
 });
+
 //Step 3 : List all user with authentication
+app.get("/list-all-users", authenticate, async (req, res) => {
+  console.log(req.user);
+  try {
+    const UsersList = await UserModel.find();
+
+    //Validation
+    if (!UsersList) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No Users Found!" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Users fetched success!",
+      users: UsersList,
+    });
+  } catch (error) {
+    return req.status(500).json({ success: false, message: "Internal Server" });
+  }
+});
 
 // Database Connection
 DBConnector();
