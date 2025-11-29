a60f785c9907fc1f30de58478d3c08479210e747
Arun-DevCode <arunkumar.d@entri.me>
2025-11-29 23:31:59 +0530

    updated

--- B40 Session/React JS/src/main.jsx
+++ B40 Session/React JS/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+// import CustomNotification from "./Notification";
+// import { Profile, UpdateForm } from "./Profile";
+import ComponentA from "./componentA";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <div>
+    <ComponentA />
+  </div>
+);
