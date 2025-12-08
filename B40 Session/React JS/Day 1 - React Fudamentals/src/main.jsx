ed4976d15ec4d2b42e1c1b79d020c286ed0da4b9
Arun-DevCode <arunkumar.d@entri.me>
2025-12-08 20:00:43 +0530

    List & events

--- B40 Session/React JS/Day 1 - React Fudamentals/src/main.jsx
+++ B40 Session/React JS/Day 1 - React Fudamentals/src/main.jsx
@@ -3,29 +3,7 @@ import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
 
 const isUserRegistered = "Guest-User";
+import Products from "./components/Products";
+import Form from "./components/Form";
 
-createRoot(document.getElementById("root")).render(
-  <div>
-    {isUserRegistered === true ? (
-      <div>
-        <h1>Login Form:</h1>
-        <LoginForm />
-      </div>
-    ) : isUserRegistered === "Guest-User" ? (
-      <div>
-        <h1>Guest User </h1>
-        <p>You logged in as Guest User</p>
-        <div>
-          <span>Register</span>
-          {" "}
-          <span>Login</span>
-        </div>
-      </div>
-    ) : (
-      <div>
-        <h1>Create Account:</h1>
-        <RegisterForm />
-      </div>
-    )}
-  </div>
-);
+createRoot(document.getElementById("root")).render(<Form />);
