72f4906c97592cfeeabeea0be5f155aa5f308e8e
Frontend-Engineer-Dev <dev.arunengineer@gmail.com>
2026-02-16 15:27:48 +0530

    updated

--- B40 Session/Backend/day-5-express-application/src/routers/userRoute.js
+++ B40 Session/Backend/day-5-express-application/src/routers/userRoute.js
@@ -5,6 +5,8 @@ import {
   createUserAccount,
   UserLogin,
 } from "../controllers/user.controller.js";
+import UserDataValidation from "../validators/users.validator.js";
+import ValidatorHandler from "../middleware/validator.js"; // Incoming Data Validate
 
 // Router Setup
 const UserRouter = express.Router();
@@ -12,7 +14,12 @@ const UserRouter = express.Router();
 // User Routes
 
 // 1. Create User Account
-UserRouter.post("/create-account", createUserAccount);
+UserRouter.post(
+  "/create-account",
+  UserDataValidation,
+  ValidatorHandler,
+  createUserAccount,
+);
 
 // 2. Access User Account
 UserRouter.post("/login", UserLogin);
